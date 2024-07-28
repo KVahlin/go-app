b6ff7748126cd7e99a632cada74429838f597c60
kvahlin <kirill.vahlin@gmail.com>
2024-07-28 23:07:17 +0400

    first commit

---
+++ src/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func MessageHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var requestData map[string]string
+	if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	textMessage := requestData["message"]
+
+	if textMessage != "" {
+		w.Write([]byte("Message: " + textMessage))
+	} else {
+		w.Write([]byte("please, input text message"))
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/message", MessageHandler)
+
+	err := http.ListenAndServe(":6030", mux)
+	log.Fatal(err)
+}
